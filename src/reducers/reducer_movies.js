f7346454ed308bec1e9c71374fe5be5e20c4a78e
Jon Wilson <jonwilsoncomedy@gmail.com>
2017-06-26 23:48:05 -0500

    adding first reducer

---
+++ src/reducers/reducer_movies.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'The Edge of Seventeen'},
+        { title: 'Into the Forest'},
+        { title: 'Captain Fantastic'},
+        { title: 'Hamlet'}
+    ]
+}
